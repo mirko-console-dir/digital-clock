f28fbaaf47689e67c9eb60b5c4abcf9a9528f72d
mirko-console-dir <cdmirko.saponaro@gmail.com>
2022-07-22 11:47:25 +1000

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Clock from "./Clock.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>World Clock</h1>
+      </div>
+      <Clock show country="ITALY" timezone="2" />
+    </>
   );
 }
 
